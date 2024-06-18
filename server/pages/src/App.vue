e523a99666745467bd75451124aca97eec84363e
AnqiNie <131529689+AnqiNie@users.noreply.github.com>
2024-06-18 18:13:54 +0800

    conection

--- server/pages/src/App.vue
+++ server/pages/src/App.vue
@@ -4,14 +4,14 @@
       <div style="background-color:#b2e1f8;" class="card">
         <div>
           <h4 style="color: rgb(115, 114, 114);font-weight: 400;">磁盘I/O量</h4>
-          <h2>{{perfData.block_io}}/s</h2>
+          <h2>{{perfData.disk_read_bytes}}/{{perfData.disk_write_bytes}}/s</h2>
         </div>
         <img src="../images/cd.IO.png" class="icon">
       </div>
       <div style="background-color:#cef3d4;" class="card">
         <div>
           <h4 style="color: rgb(115, 114, 114);font-weight: 400;">网络I/O量</h4>
-          <h2>{{perfData.net_io}}/s</h2>
+          <h2>{{perfData.recv_bytes}}/{{perfData.sent_bytes}}/s</h2>
         </div>
         <img src="../images/intel.IO.png" class="icon">
       </div>
@@ -22,7 +22,7 @@
         <div style="background-color:#F6F7E7;" class="card">
           <div>
             <h4 style="color: rgb(115, 114, 114);font-weight: 400;">磁盘读写次数</h4>
-            <h2>{{ perfData.block_io_cnt }}/s</h2>
+            <h2>{{ perfData.disk_read_count}}/{{perfData.disk_write_count }}/s</h2>
           </div>
           <img src="../images/cd.rw.png" class="icon">
 
@@ -30,7 +30,7 @@
         <div style="background-color:#FFEDE6;" class="card">
           <div>
             <h4 style="color: rgb(115, 114, 114);font-weight: 400;">网络读写次数</h4>
-            <h2>{{ perfData.net_io_cnt }}/s</h2>
+            <h2>{{ perfData.recv_count }}/{{perfData.sent_count}}/s</h2>
           </div>
           <img src="../images/intel.rw.png" class="icon">
         </div>
@@ -40,11 +40,19 @@
   
   <div>
     <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="pid" label="pid" width="380">
+      </el-table-column>
       <el-table-column prop="name" label="进程" width="380">
       </el-table-column>
-      <el-table-column prop="block_io" label="磁盘I/O量" width="380">
+      <el-table-column prop="disk_read_bytes" label="磁盘读写量" width="380">
       </el-table-column>
-      <el-table-column prop="net_io" label="网络I/O量">
+      <el-table-column prop="disk_write_bytes" label=" " width="380">
+      </el-table-column>
+      <el-table-column prop="sent_bytes" label="网络I/O量">
+      </el-table-column>
+      <el-table-column prop="recv_bytes" label=" ">
+      </el-table-column>
+      <el-table-column prop="cpu_usage" label="cpu使用量">
       </el-table-column>
     </el-table>
   </div>
@@ -59,18 +67,22 @@ export default {
         return {
           tableData: [],
           perfData: {
-            cpu: '--',
-            memory: '--',
-            block_io: '--',
-            block_io_cnt: '--',
-            net_io: '--',
-            net_io_cnt: '--'
+            sent_count: '--',
+            recv_count: '--',
+            sent_bytes: '--',
+            recv_bytes: '--',
+            disk_read_bytes: '--',
+            disk_write_bytes: '--',
+            disk_read_count: '--',
+            disk_write_count: '--',
+            cpu_usage: '--'
           }
         };
     },
     methods: {
-      async fetchData(url){  
-        try {
+      async fetchData(url) { 
+              // 执行需要轮询的操作 
+          try {
           const response = await fetch(url); // 等待fetch完成并返回结果  
           const data = await response.json(); // 等待解析响应数据并返回结果  
           console.log(data); // 打印解析后的数据
@@ -78,16 +90,24 @@ export default {
         } catch (error) {  
           console.error('Error:', error); // 捕获并打印错误信息  
         }  
-      }
+        },
     },
     async mounted(){
       setInterval(() => {
-        this.tableData = await this.fetchData('/api/proc');
-        var data= await this.fetchData('/api/perf');
-        this.perfData=data;
-      }, 1000);
-    }
+        
+      }, 1000); 
+          // 每隔一段时间执行某个方法 
+          this.pollingTimer = setInterval(() => { this.fetchData(); 
+            // 调用需要轮询的方法 
+            this.tableData = await this.fetchData('/api/proc');
+            var data= await this.fetchData('/api/perf');
+            this.perfData=data;
+            }, 1000); // 1秒为例，可以根据需求调整时间间隔 
+            },
+  
+     
 };
+}
 </script>
 
 <style scoped>
