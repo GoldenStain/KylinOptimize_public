99385c1294a66cc630e60a89e29945d0959e37a2
wsw <wsw@localhost.localdomain>
2024-07-13 19:09:59 +0800

    added memory tuning

--- program/examples/tuning/memory/prepare.sh
+++ program/examples/tuning/memory/prepare.sh
@@ -14,5 +14,3 @@ sed -i "s#.stream/Makefile# $cur/Makefile#g" $cur/tuning_stream_server.yaml
 echo "copy the server yaml file to /etc/atuned/tuning/"
 cp tuning_stream_server.yaml /etc/atuned/tuning/
 
-
-wget http://www.cs.virginia.edu/stream/FTP/Code/stream.c
