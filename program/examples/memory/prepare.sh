4876ddee808c87df70067d406a38d5726d351d29
mindkinghub <mindking218@gmail.com>
2024-07-29 17:54:49 +0800

    added stream.c

--- program/examples/memory/prepare.sh
+++ program/examples/memory/prepare.sh
@@ -13,5 +13,3 @@ sed -i "s#.stream/Makefile# $cur/Makefile#g" $cur/tuning_stream_server.yaml
 
 echo "copy the server yaml file to /etc/atuned/tuning/"
 cp tuning_stream_server.yaml /etc/atuned/tuning/
-
-# wget http://www.cs.virginia.edu/stream/FTP/Code/stream.c
